bc97bb248cee8dbaec4b85b70c6af4b5395192cf
moradiii <mourad.mahmoudi@uir.ac.ma>
2024-04-15 11:04:41 +0100

    Mourad

---
+++ demo/src/main/java/com/example/demo/modele/Comment.java
@@ -0,0 +1,44 @@
+package com.example.demo.modele;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    public Comment() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "Comment{" +
+                "id=" + id +
+                ", content='" + content + '\'' +
+                '}';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
