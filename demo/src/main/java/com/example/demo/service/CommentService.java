42b1e76ecd2ea7fccb9c4cb63a7582957c6c3203
argui <adnane142003@gmail.com>
2024-04-16 12:17:40 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	demo/src/main/java/com/example/demo/service/ProduitService.java

--- demo/src/main/java/com/example/demo/service/CommentService.java
+++ demo/src/main/java/com/example/demo/service/CommentService.java
@@ -6,11 +6,12 @@ import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
-@Repository
+@Service
 public class CommentService {
     public CommentService(CommentRepository CommentRepository) {
         this.CommentRepository = CommentRepository;
