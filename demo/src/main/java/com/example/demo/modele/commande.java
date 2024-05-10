3ba560a71dfbb0c9046e056d5d8b1135ddd18e70
issam <issam.sbabou2002@gmail.com>
2024-05-11 00:36:23 +0100

    iss

--- demo/src/main/java/com/example/demo/modele/commande.java
+++ demo/src/main/java/com/example/demo/modele/commande.java
@@ -2,6 +2,7 @@ package com.example.demo.modele;
 import jakarta.persistence.*;
 
 import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Table(name ="commands")
@@ -18,5 +19,74 @@ public class commande {
     private String lieuLivraison;
     @Column
     private String méthodePaiement;
+    @Column
+    private Boolean delivered;
 
+    public commande() {
+    }
+    public commande(int id, comptes compte, String dateLivraison, String lieuLivraison, String méthodePaiement, Boolean delivered) {
+        this.id = id;
+        this.compte = compte;
+        this.dateLivraison = dateLivraison;
+        this.lieuLivraison = lieuLivraison;
+        this.méthodePaiement = méthodePaiement;
+        this.delivered = delivered;
+    }
+    public int getId() {
+        return id;
+    }
+    public comptes getCompte() {
+        return compte;
+    }
+    public String getDateLivraison() {
+        return dateLivraison;
+    }
+    public String getLieuLivraison() {
+        return lieuLivraison;
+    }
+    public String getMéthodePaiement() {
+        return méthodePaiement;
+    }
+    public Boolean isDelivered() {
+        return delivered;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setCompte(comptes compte) {
+        this.compte = compte;
+    }
+    public void setDateLivraison(String dateLivraison) {
+        this.dateLivraison = dateLivraison;
+    }
+    public void setLieuLivraison(String lieuLivraison) {
+        this.lieuLivraison = lieuLivraison;
+    }
+    public void setMéthodePaiement(String méthodePaiement) {
+        this.méthodePaiement = méthodePaiement;
+    }
+    public void setDelivered(Boolean delivered) {
+        this.delivered = delivered;
+    }
+    @Override
+    public String toString() {
+        return "commande{" +
+                "id=" + id +
+                ", compte=" + compte +
+                ", dateLivraison='" + dateLivraison + '\'' +
+                ", lieuLivraison='" + lieuLivraison + '\'' +
+                ", méthodePaiement='" + méthodePaiement + '\'' +
+                '}';
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        commande commande = (commande) o;
+        return id == commande.id && Objects.equals(compte, commande.compte) && Objects.equals(dateLivraison, commande.dateLivraison) && Objects.equals(lieuLivraison, commande.lieuLivraison) && Objects.equals(méthodePaiement, commande.méthodePaiement);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, compte, dateLivraison, lieuLivraison, méthodePaiement);
+    }
 }
