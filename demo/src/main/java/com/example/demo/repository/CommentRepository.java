48f54a250d25315928837ff3219eaf704d8b30a2
moradiii <mourad.mahmoudi@uir.ac.ma>
2024-05-14 23:29:01 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/misc.xml

--- demo/src/main/java/com/example/demo/repository/CommentRepository.java
+++ demo/src/main/java/com/example/demo/repository/CommentRepository.java
@@ -2,8 +2,21 @@ package com.example.demo.repository;
 
 import com.example.demo.modele.Comment;
 import com.example.demo.modele.categorie;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    @Override
+    Optional<Comment> findById(Long aLong);
+
+    @Override
+    List<Comment> findAllById(Iterable<Long> longs);
+
+    @Override
+    List<Comment> findAll(Sort sort);
 }
